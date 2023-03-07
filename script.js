c34710dcb27517e0948e7fd495d28c9419cd8019
Romanrodriguez-proyectofinal <127217797+Romanrodriguez-proyectofinal@users.noreply.github.com>
2023-03-07 20:57:24 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("cambio-color").addEventListener('click', function () {
+    document.getElementById("lado-izquierdo").style.background = 'purple';
+});
+
+document.getElementById("cambio-letra").addEventListener('click', function () {
+    document.body.style.fontSize = 'large';
+});
+
+document.getElementById("color-letra").addEventListener('click', function () {
+    document.body.style.color = 'red';
+});
